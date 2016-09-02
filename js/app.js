872577f97f00fce4b51edf83207b959a9a9a6ff6
Neil Ferner <n.e.ferner@gmail.com>
2016-09-02 10:47:53 -0400

    add function and click event

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.onload = function()
+
+function chipColor() {
+  this.chipChange = document.querySelectorAll('chip');
+  }
+  if (chipChange.style.backgroundColor = 'white') {
+    chipChange.style.backgroundColor = 'black';
+  }
+}
+chipChange.addEventListener('click', function chipColor());
