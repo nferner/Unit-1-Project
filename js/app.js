d21296fefe4dd3d1accf64d33249c9965223f37e
Neil Ferner <n.e.ferner@gmail.com>
2016-09-02 13:48:25 -0400

    turn a chip black via javascript

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 function chipColor () {
-  if (document.querySelectorAll('.chip').style.backgroundColor = 'white')
-  return document.querySelectorAll('.chip').style.backgroundColor = 'black'
+  document.querySelector('.chip').style.backgroundColor = 'black'
 }
 
 window.onload = function () {
-  document.querySelectorAll('.chipClick').addEventListener('click', chipColor())
+  document.querySelector('.chip').addEventListener('click', chipColor)
 }
+/* class chip () {
+  constructor () {
+ this.el = document.querySelectorAll('.chip').style.background = 'black'
+  }
+}
+ function chipColor () {
+   if (document.querySelectorAll('.chip').style.backgroundColor = 'white')
+ }
 
-/*  if (chipChange.style.backgroundColor = 'white') {
+  window.onload = function () {
+    document.querySelectorAll('.chipClick').addEventListener('click', chip)
+  }
+
+ if (chipChange.style.backgroundColor = 'white') {
     chipChange.style.backgroundColor = 'black';
   }
 }
