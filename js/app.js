053ebe191055c6aadc5db8e8ca1986edc169c954
Neil Ferner <n.e.ferner@gmail.com>
2016-09-04 11:36:32 -0400

    add pseudo code for chip - board placement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
+/*  need to make each space accesible for click to turn black
+    need to make each space accesible for each click to turn chip red
+    need to order the clicks for either after one you get the other or by how many clicks
+    for each odd click of the array with the class of chip the 'chip' should turn black
+    for each even click of the array with the class of chip the 'chip' should turn red
+    the 'chip' can only turn red or black if the 'chip' is white
+    this 'chip' can only turn red or black if the 'chip' is above another red or black chip
+    in order for a 'chip' to be above a red or black 'chip' the column is the same
+    the row is one place higher
+    I think what i need is an array for each column
+    all inside an array for the grid as a whole
+    something that looks like first move: [i][j]
+    the next possible move could be [i][j--], [i++][j] thru [i+8][j]
+    maybe something about .length goes in there
+*/
+
+
+
+
+
 function chipColorBlack () {
+ for (let chip of chipss)
   document.querySelector('.chip').style.backgroundColor = 'black'
 }
 // change to rows and maybe get element by id and change children...which are now columns
